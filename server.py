f621cf6ca28363f9c269389633ea6d33d7aa507c
Veneta <venkireva80@gmail.com>
2022-12-28 19:36:37 +0200

    Reformat client message

--- server.py
+++ server.py
@@ -89,7 +89,7 @@ def serve(server):
                             # Sort the list
                             sorted_list = quicksort_helper(message_eval[1], message_eval[0])
                             # Put message into message queue
-                            messages_per_client[obj].put('[' + ' '.join([str(i) for i in sorted_list]) + ']')
+                            messages_per_client[obj].put('[' + ','.join([str(i) for i in sorted_list]) + ']')
                     except (ValueError, SyntaxError):
                         print('Please provide input in the following format:')
                         print('(<number of processes>, <list to sort>)')
