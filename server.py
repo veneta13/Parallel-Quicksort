dfa0b00c8de225ee7997022b3c7e580d09cd12ca
Veneta <venkireva80@gmail.com>
2022-12-28 19:04:48 +0200

    Add quicksort to input

--- server.py
+++ server.py
@@ -1,7 +1,29 @@
 import socket
+from multiprocessing import Process, Pipe
 from queue import Queue
 
 import select
+
+from quicksort_parallel import quicksort_parallel
+
+
+def quicksort_helper(input_list, proc_count):
+    p_pipe, c_pipe = Pipe(duplex=False)
+    proc = Process(
+        target=quicksort_parallel,
+        args=(
+            input_list,
+            c_pipe,
+            proc_count,
+            1
+        )
+    )
+
+    proc.start()
+    output = p_pipe.recv()
+    proc.join()
+    proc.close()
+    return output
 
 
 def create_server(host, port):
@@ -14,6 +36,7 @@ def create_server(host, port):
     # Bind to host and port
     server.bind((host, port))
 
+    # Listen
     server.listen()
 
     return server
@@ -54,28 +77,37 @@ def serve(server):
                 # Receive message
                 message = obj.recv(1024)
                 if message:
-
                     if obj not in messages_per_client.keys():
                         messages_per_client[obj] = Queue()
-
                     try:
-                        print(eval(message))
+                        message_eval = eval(message)
+
+                        if not isinstance(message_eval, tuple) or not isinstance(message_eval[1], list):
+                            print('Please provide input in the following format:')
+                            print('(<number of processes>, <list to sort>)')
+                        else:
+                            # Sort the list
+                            sorted_list = quicksort_helper(message_eval[1], message_eval[0])
+                            # Put message into message queue
+                            messages_per_client[obj].put('[' + ' '.join([str(i) for i in sorted_list]) + ']')
                     except (ValueError, SyntaxError):
-                        pass
+                        print('Please provide input in the following format:')
+                        print('(<number of processes>, <list to sort>)')
 
-                    messages_per_client[obj].put(message)
-
-                    # Add to writes
+                    # Add client to writes
                     if obj not in writes:
                         writes.append(obj)
 
         for obj in write_sel:
             try:
+                # Get message for current client
                 message = messages_per_client[obj].get_nowait()
             except:
+                # If there are no massage for that client remove from writes
                 writes.remove(obj)
             else:
-                obj.send(message)
+                # If there are messages waiting send them
+                obj.send(message.encode())
 
 
 def main():
