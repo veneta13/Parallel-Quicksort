cc63eeb2c7a22145df3eaabc40f10e1b67693935
Veneta <venkireva80@gmail.com>
2022-12-28 19:37:09 +0200

    Read user input and add main

--- client.py
+++ client.py
@@ -17,9 +17,37 @@ def send_client(client, message):
 
 
 def receive_client(client):
-    message = client.recv(1024)
+    message = client.recv(1024).decode()
 
     if not message:
         client.close()
         return None
+
+    print(f'Received a message: {message}')
     return message
+
+
+def client_user_input():
+    proc_count = int(input('Enter process count: '))
+
+    elem_count = int(input('Enter number of elements: '))
+    input_list = list(map(int, input('Enter the list: ').strip().split()))[:elem_count]
+
+    result = '('
+    result += str(proc_count)
+    result += ', '
+    result += '['
+    result += ','.join([str(i) for i in input_list])
+    result += '])'
+
+    return result
+
+
+def main():
+    client = create_client('localhost', 8008)
+    send_client(client, client_user_input())
+    receive_client(client)
+
+
+if __name__ == "__main__":
+    main()
